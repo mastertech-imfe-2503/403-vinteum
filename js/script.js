84ca03af22e51760c247e0cbc2f4493f7e36a5b2
Renan Rodrigo <renanrb@ime.usp.br>
2019-04-18 19:10:06 -0300

    Add function to get new deck

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const inputs = document.querySelectorAll('input');
 const firstPage = document.querySelector('.first-page');
 const secondPage = document.querySelector('.second-page');
 
+let deckID;
+
 let players = [
   {
     name: '',
@@ -38,6 +40,7 @@ function validateNames(){
 
 function startGame(){
   sortNames();
+  getDeck();
   // pegar duas cartas para cada um
   // somar os pontos de cada um e colocar na tela
 }
@@ -55,4 +58,15 @@ function sortNames(){
 
   document.querySelector('.player1 .name').innerHTML = players[0].name;
   document.querySelector('.player2 .name').innerHTML = players[1].name;
+}
+
+function getDeck(){
+  fetch("http://deckofcardsapi.com/api/deck/new/shuffle/")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    deckID = json.deck_id;
+    console.log(deckID);
+  })
 }
